d132ada0735dd7f65cf216333fa4a0552d0361ea
EpicMo <1982742309@qq.com>
2023-10-26 16:00:39 +0800

    fix: fix args

--- train_api.py
+++ train_api.py
@@ -37,16 +37,17 @@ async def train_network(websocket: WebSocket):
         logger.info("Got task, starting train with data" + data["train_data"])
         parser = train_network_api.setup_parser()
         output_name = str(uuid.uuid4())
-        args = parser.parse_args()
-        args.train_data_dir = pathlib.Path.joinpath("train", data["train_data"])
-        args.resolution = data["resolution"]
+
+        args_append = ["--train_data_dir", pathlib.Path.joinpath("train", data["train_data"]),
+                       "--resolution", data["resolution"]]
         if data["is_v2_model"]:
-            args.v2 = True
-        args.pretrained_model = pathlib.Path.joinpath(
+            args_append.append("--v2")
+        args_append.append(["--pretrained_model", pathlib.Path.joinpath(
             "../sd-models", data["model"] + ".safetensors"
-        )
-        args.output_name = output_name
-        args = prepare_hardcoded_args(args, data["train_data"])
+        )])
+        args_append.append(["--output_name", output_name])
+        args_append = prepare_hardcoded_args(args_append, data["train_data"])
+        args = parser.parse_args(args_append)
         await trainer.train(args, websocket)
         await websocket.send_text(
             json.dumps(
@@ -59,40 +60,42 @@ async def train_network(websocket: WebSocket):
         )
 
 
-def prepare_hardcoded_args(args, model):
-    args.enable_bucket = True
-    args.logging_dir = "./logs"
-    args.log_prefix = model
-    args.network_module = "networks.lora"
-    args.max_train_epochs = 10
-    args.learning_rate = "1e-4"
-    args.unet_lr = "1e-4r"
-    args.text_encoder = "1e-5"
-    args.lr_scheduler = "cosine_with_restarts"
-    args.lr_warmup_steps = 0
-    args.lr_scheduler_num_cycles = 1
-    args.network_dim = 128
-    args.network_alpha = 128
-    args.train_batch_size = 1
-    args.save_every_n_epochs = 1
-    args.mixed_precision = "fp16"
-    args.save_precision = "fp16"
-    args.seed = "1337"
-    args.cache_latents = True
-    args.prior_loss_weight = 1
-    args.max_token_length = 225
-    args.caption_extension = ".txt"
-    args.save_model_as = "safetensors"
-    args.min_bucket_reso = 256
-    args.max_bucket_reso = 1024
-    args.keep_tokens = 0
-    args.xformers = True
-    args.shuffle_caption = True
-    args.output_dir = '../output'
+def prepare_hardcoded_args(args, model) -> list[str]:
+    args.append([
+        "--enable_bucket",
+        "--logging_dir", ".logs",
+        "--log_prefix", model,
+        "--network_module", "networks.lora",
+        "--max_train_epochs", "10",
+        "--learning_rate", "1e-4",
+        "--unet_lr", "1e-4r",
+        "--text_encoder", "1e-5",
+        "--lr_scheduler", "cosine_with_restarts",
+        "--lr_warmup_steps", "0",
+        "--lr_scheduler_num_cycles", "1",
+        "--network_dim", "128",
+        "--network_alpha", "128",
+        "--train_batch_size", "1",
+        "--save_every_n_epochs", "1",
+        "--mixed_precision", "fp16",
+        "--save_precision", "fp16",
+        "--seed", "1337",
+        "--cache_latents",
+        "--prior_loss_weight", "1",
+        "--max_token_length", "225",
+        "--caption_extension", ".txt",
+        "--save_model_as", "safetensors",
+        "--min_bucket_reso", "256",
+        "--max_bucket_reso", "1024",
+        "--keep_tokens", "0",
+        "--xformers",
+        "--shuffle_caption",
+        "--output_dir", '../output'
+    ])
     return args
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=int(sys.argv[1]))
+    uvicorn.run(app, host="127.0.0.1", port=8999)
